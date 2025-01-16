c1fc1f8bd5c879aae685f317d304dd5bf06ef447
Anant3008 <sastrulaanant@gmail.com>
2025-01-16 13:40:36 +0530

    added login page

--- FRONTEND/my-react-app/src/App.jsx
+++ FRONTEND/my-react-app/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { BrowserRouter , Routes, Route } from "react-router-dom";
-import {LoginPage} from './Routes/Route.js';
+import {LoginPage,SignupPage} from './Routes/Route.js';
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage/>}></Route>
     </Routes>
     </BrowserRouter>  
   )
