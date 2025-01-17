b12bef9f84792bc1de4db16932e918f2453478cf
Anant3008 <sastrulaanant@gmail.com>
2025-01-17 16:21:54 +0530

    mior changes

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,6 +1,6 @@
 const express =require('express')
 const app=express()
-const user=require('./controller/userController')
+const user=require('./controller/user')
 const bodyParser=require("body-parser")
 const cors=require("cors")
 const ErrorHandler=require("./middleware/error")
