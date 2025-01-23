3af6440da867daf5248d2b171c1e1e96c27e219d
Anant3008 <sastrulaanant@gmail.com>
2025-01-23 15:55:07 +0530

    milestone 7 added login backend

--- BACKEND/controller/user.js
+++ BACKEND/controller/user.js
@@ -18,7 +18,7 @@ router.post("/create-user", upload.single("file"), catchAsyncErrors( async (req,
     const userEmail = await User.findOne({ email });
     if (userEmail) {
         if (req.file){
-            const filePath = path.join(__dirname , "../uploads"/req.file.filename) ;
+            const filePath = path.join(__dirname , "../uploads",req.file.filename) ;
             try{
                 fs.unlinkSync(filePath);
             }
@@ -52,5 +52,44 @@ router.post("/create-user", upload.single("file"), catchAsyncErrors( async (req,
     res.status(201).json({ success : true , user });
 }));
 
+
+
+router.post('/login-user',catchAsyncErrors(async (req,res,next)=>{
+    console.log("Logging in user...")
+
+    let {email , password}=req.body
+    email=email
+    password=password
+
+    if(!email || !password){
+        return next(new ErrorHandler("Provide both email and password",400))
+    }
+
+    const user_auth=await User.findOne({email}).select('+password')
+
+    if(!user_auth){
+        return next(new ErrorHandler("Please signup as we dont have ur account",401))
+    }
+
+    const isPasswordMatched=await bcrypt.compare(password, user_auth.password);
+    console.log("Matched ?",isPasswordMatched)
+
+
+
+    if (!isPasswordMatched){
+        console.log("Password not match")
+        return next(new ErrorHandler("Invalid password",401))
+    }
+    res.status(200).json({
+        success:true,
+        message:"Login successful",
+        user:{
+            id: user_auth.id,
+            name: user_auth.name
+        }
+    }
+    );
+
+}))
 
 module.exports = router;
